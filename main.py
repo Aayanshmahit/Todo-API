2f66b6ab4f9ec7adfdc2c889dfccf49473c6106c
Aayanshmahit <aayanshmahit61@gmail.com>
2026-09-08 03:50:01 +0530

    completed rest api

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ from validate import ValidInputs
 
 app = FastAPI()
 
-post_num = 0
+post_num = 1
 post_store = {}
 
 @app.post("/posts")
 def create_post(data : ValidInputs):
     global post_num
     response = {
-        "id" : post_num+1 ,
         "title" : data.title , 
         "description" : data.description,
         "completion" : data.completed
     }
-    post_num+=1
     post_store[post_num] = response
+    post_num+=1
     return response
 
 @app.get("/posts")
@@ -24,13 +23,26 @@ def get_post():
     return post_store
 
 @app.get("/posts/{id}")
-def get_post_id():
-    pass
+def get_post_id(id : int):
+    if id not in post_store:
+        raise HTTPException(status_code = 404 , detail = "Post not Found")
+    return post_store.get(id)
 
-@app.delete("/posts")
-def delete_post():
-    pass
+@app.delete("/posts/{id}")
+def delete_post(id : int):
+    if id not in post_store:
+        raise HTTPException(status_code = 404 , detail = "Post not Found")
+    del post_store[id]
+    return "Post deleted sucessfully"
+    
 
 @app.put("/posts/{id}")
-def update_post():
-    pass
+def update_post(id : int , data : ValidInputs):
+    if id not in post_store:
+        raise HTTPException(status_code = 404 , detail = "Post not Found")
+    post_store[id] = {
+        "title" : data.title , 
+        "description" : data.description,
+        "completion" : data.completed
+    }
+    return post_store[id]
--- validate.py
+++ validate.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel
 from typing import Optional
 
 class ValidInputs(BaseModel):
-    id : int
     title : str
     description : str
     completed : Optional[bool] = False
